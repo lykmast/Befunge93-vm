d75c42907bcde915dee47ed030db8b7f1854646c
lykmast <lykmast@gmail.com>
2020-03-20 16:16:54 +0200

    corrected definitions

--- vm.h
+++ vm.h
@@ -1,23 +1,17 @@
 #define DEFAULT_STACK_SIZE 1000
 #define ROWS 25
 #define COLS 80
-#define LEFT 1
-#define RIGHT -1
-#define UP -COLS
-#define DOWN +COLS
 typedef enum {NORTH,EAST,SOUTH,WEST} dir_t;
 typedef struct {
-	char grid[ROWS][COLS];
-	int pcI;
-	int pcJ;
+	char (*grid)[COLS];
 	char stack[DEFAULT_STACK_SIZE];
-	int sp;
-	dir_t dir;
 } VM;
 
-VM *vm_create(char** grid);
-VM *vm_free(VM *vm);
-VM *vm_init(VM* vm, char **grid);
-void vm_exec(VM *vm, int startI, int startJ, dir_t dir, bool trace);
-void vm_print_instr(int *code, int pcI, int pcJ);
-void vm_print_stack(int *stack, int count);
+VM *vm_create(char grid[ROWS][COLS]);
+void vm_free(VM *vm);
+void vm_init(VM *vm, char grid[ROWS][COLS]);
+void vm_exec(VM *vm, int startI, int startJ, dir_t start_dir, int trace);
+void vm_print_instr(char grid[ROWS][COLS], int pcI, int pcJ);
+void vm_print_stack(char *stack, int count);
+void vm_print_grid(VM *vm);
+void vm_print_state(VM *vm, int pcI, int pcJ, dir_t dir, int sp);
